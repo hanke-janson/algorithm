eb3bc7a7bd4d739b126911ea17a65a39af8a102e
hanke-janson <1048458653@163.com>
2021-09-01 17:35:26 +0800

    小和问题

---
+++ two/src/topic2/SmallSum.java
@@ -0,0 +1,135 @@
+package topic2;
+
+/**
+ * 小和问题
+ */
+public class SmallSum {
+
+    public static int smallSum(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return 0;
+        }
+        return mergeSort(arr, 0, arr.length - 1);
+    }
+
+    //arr[l...r]既要排好序，也要求小和
+    public static int mergeSort(int[] arr, int l, int r) {
+        if (l == r) {
+            return 0;
+        }
+        int mid = l + ((r - l) >> 1);//求中点
+        return mergeSort(arr, l, mid)//左侧排序求小和的数量
+                + mergeSort(arr, mid + 1, r)//右侧排序求小和的数量
+                + merge(arr, l, mid, r);//左右排好合并时小和的数量
+    }
+
+    public static int merge(int[] arr, int l, int m, int r) {
+        int[] help = new int[r - l + 1];//初始化一个辅助数组，大小为l到r上的个数
+        int i = 0;//help数组下标
+        int p1 = l;//arr数组前半段下标
+        int p2 = m + 1;//arr数组后半段下标
+        int res = 0;
+        while (p1 <= m && p2 <= r) {
+            //merge过程中算小和
+            //前半段小于后半段时产生小和增加的行为
+            //r - p2 + 1 :当前后半段有多少数比p1大
+            res += arr[p1] < arr[p2] ? (r - p2 + 1) * arr[p1] : 0;
+            help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];//相等时先拷贝右组
+        }
+        while (p1 <= m) {
+            help[i++] = arr[p1++];
+        }
+        while (p2 <= r) {
+            help[i++] = arr[p2++];
+        }
+        for (i = 0; i < help.length; i++) {
+            arr[l + i] = help[i];
+        }
+        return res;
+    }
+
+    // for test
+    public static int comparator(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return 0;
+        }
+        int res = 0;
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 0; j < i; j++) {
+                res += arr[j] < arr[i] ? arr[j] : 0;
+            }
+        }
+        return res;
+    }
+
+    // for test
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    // for test
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    // for test
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // for test
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            if (smallSum(arr1) != comparator(arr2)) {
+                succeed = false;
+                printArray(arr1);
+                printArray(arr2);
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+    }
+
+}
